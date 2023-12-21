24bb287a5871dae641d5ec9e533c1688069c0833
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2023-12-21 19:03:04 +0530

    changed the border color for active window

--- config.def.h
+++ config.def.h
@@ -20,11 +20,12 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_brown[]		= "#361a09";
+static const char col_brown[]				= "#361a09";
+static const char dracula_purple[]	= "#bd93f9";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1,  col_brown  },
+	[SchemeSel]  = { col_gray4, col_gray1, dracula_purple },
 };
 
 static const char *const autostart[] = {
