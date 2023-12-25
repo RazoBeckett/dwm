aea798bb39f42c2c47b78dd9068ca363c5011799
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2023-12-25 19:17:30 +0530

    Added gaps around just one open window alongside gaps between multiple windows.
    Refactor the tile function to dynamically adjust the gap between windows based on the number of clients. The function now calculates whether to use gappx or fgappx as the gap size, depending on whether there is only one client or multiple clients on the monitor.

--- dwm.c
+++ dwm.c
@@ -2484,30 +2484,36 @@ tagmon(const Arg *arg)
 void
 tile(Monitor *m)
 {
-	unsigned int i, n, h, mw, my, ty;
-	Client *c;
+    unsigned int i, n, h, mw, my, ty, ns;
+    Client *c;
 
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if (n == 0)
-		return;
+    for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+    if (n == 0)
+        return;
 
-	if (n > m->nmaster)
-		mw = m->nmaster ? m->ww * m->mfact : 0;
-	else
-		mw = m->ww;
-	for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < m->nmaster) {
-			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
-			resize(c, m->wx, m->wy + my, mw - (2*c->bw) + (n > 1 ? gappx : 0), h - (2*c->bw), 0);
-			if (my + HEIGHT(c) < m->wh)
-				my += HEIGHT(c);
-		} else {
-			h = (m->wh - ty) / (n - i);
-			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
-			if (ty + HEIGHT(c) < m->wh)
-				ty += HEIGHT(c);
-		}
+    unsigned int gap = (n > 1) ? gappx : fgappx;  // Choose gap based on the number of clients
+
+    if (n > m->nmaster) {
+        mw = m->nmaster ? m->ww * m->mfact : 0;
+        ns = m->nmaster > 0 ? 2 : 1;
+    } else {    
+        mw = m->ww;
+        ns = 1;
+    }
+
+    for(i = 0, my = ty = gap, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+        if (i < m->nmaster) {
+            h = (m->wh - my) / (MIN(n, m->nmaster) - i) - gap;
+            resize(c, m->wx + gap, m->wy + my, mw - (2*c->bw) - gap*(5-ns)/2, h - (2*c->bw), False);
+            my += HEIGHT(c) + gap;
+        } else {
+            h = (m->wh - ty) / (n - i) - gap;
+            resize(c, m->wx + mw + gap/ns, m->wy + ty, m->ww - mw - (2*c->bw) - gap*(5-ns)/2, h - (2*c->bw), False);
+            if (ty + HEIGHT(c) < m->wh)
+                ty += HEIGHT(c) + gap;
+        }
 }
+
 
 void
 togglebar(const Arg *arg)
